f8fe446547632ac2ed70c41f5dd769e47f2603e1
JZ <JasonZeng@gmail.com>
2018-03-10 14:50:06 -0500

    added bubbles

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -2,3 +2,7 @@ setTimeout(addHide, 2000);
 function addHide(){
   $(".description").addClass("hide");
 }
+setTimeout(showBubble, 2000);
+function showBubble(){
+  $(".bubble--hide").removeClass("bubble--hide");
+}
